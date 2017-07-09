4662a19218b86bdca199888ffe46e30c8ff86cba
Stephen DeLorme <stephen@d.elor.me>
2017-07-08 20:18:52 -0400

    Added locations report feature

--- app/assets/js/fountain-reader.js
+++ app/assets/js/fountain-reader.js
@@ -118,7 +118,7 @@
         $app.on('fountain-js:loaded', function (e, file) {
           $dock.hide();
           $script.empty();
-          fountain.parse(file, function (result) {
+          fountain.parse(file, true, function (result) {
             if (result) {
               if (result.title && result.html.title_page) {
                 $script.append(page(result.html.title_page, true));
@@ -127,8 +127,141 @@
               $script.append(page(result.html.script));
               $workspace.fadeIn();
               notify($title.text() + ' loaded!');
-            }          
-          }); 
+            }
+
+            /*Begin statistics section */
+
+            var output          = result,
+                scene_headings  = new Array,
+                text,
+                unused          = true;
+
+            //Determine locations   
+            $(output.tokens).each(function(){
+              //Determine if we are looking at a scene heading
+              if($(this)[0].type === 'scene_heading') {
+
+                var text = $(this)[0].text;
+
+                //Determine night or day
+                if( text.indexOf(' - DAY') > 0 ) {
+                  var location = text.substring(0, text.indexOf(' - DAY'));
+                  var time = 'day';
+                } else if( text.indexOf(' - NIGHT') > 0 ) {
+                  var location = text.substring(0, text.indexOf(' - NIGHT'));
+                  var time = 'night';
+                }else {
+                  var location = text;
+                  var time = 'unknown';
+                }
+
+                $(scene_headings).each(function(){
+                  
+                  if( $(this)[0].text === location ) {
+                    
+                    if(time === 'day') {
+                      if( $(this)[0].day_count === undefined ) {
+                        $(this)[0].day_count = 1;
+                      } else {
+                        $(this)[0].day_count++;
+                      }
+                    } else if(time === 'night') {
+                      if( $(this)[0].night_count === undefined ) {
+                        $(this)[0].night_count = 1;
+                      } else {
+                        $(this)[0].night_count++;
+                      }
+                    } else {
+                      if ( $(this)[0].unknown_count === undefined ) {
+                        $(this)[0].unknown_count = 1;
+                      } else { 
+                        $(this)[0].unknown_count++;
+                      }
+                    }
+                    unused = false;
+                    return false;
+                  } else {
+                    unused = true;
+                  }
+                });
+
+                if( unused === true ) {
+                  
+                  if( time === 'day' ) {
+                    scene_headings.push({
+                      text: location,
+                      day_count: 1 
+                    });
+                  } else if( time === 'night' ) {
+                    scene_headings.push({
+                      text: location,
+                      night_count: 1 
+                    });
+                  } else {
+                    scene_headings.push({
+                      text: location,
+                      unknown_count: 1 
+                    });
+                  }
+                  
+                }
+              }
+            });
+
+            //Determining total number of scenes
+            $(scene_headings).each(function(){
+              
+              //Set undefined and NaN to 0
+              if( $(this)[0].day_count === undefined ) $(this)[0].day_count = 0;
+              if( $(this)[0].night_count === undefined ) $(this)[0].night_count = 0;
+              if( $(this)[0].unknown_count === undefined ) $(this)[0].unknown_count = 0;
+
+              var total_count = $(this)[0].day_count + $(this)[0].night_count + $(this)[0].unknown_count;
+
+              $(this)[0].total_count = total_count;
+
+              //Set undefined and NaN to 0
+              if( $(this)[0].total_count === undefined ) $(this)[0].total_count = 0;
+            });
+
+            function sort_by_count(a,b) {
+              if (a.total_count > b.total_count)
+                return -1;
+              if (a.total_count < b.total_count)
+                return 1;
+              return 0;
+            }
+
+            scene_headings.sort(sort_by_count);
+
+            $i=1;
+            
+            //Output exteriors
+            $(scene_headings).each(function(){
+              if($(this)[0].text.indexOf('EXT.') > -1) {
+                $('#locations table').append('<tr class="exterior"><td>' + $i + '</td><td>' + $(this)[0].text + '</td><td>' + $(this)[0].day_count + '</td><td>' + $(this)[0].night_count + '</td><td>' + $(this)[0].unknown_count + '</td><td>' + $(this)[0].total_count + '</td></tr>');
+                $i++;
+              }
+            });
+
+            //Output interiors
+            $(scene_headings).each(function(){
+              if($(this)[0].text.indexOf('INT.') > -1) {
+                $('#locations table').append('<tr class="interior"><td>' + $i + '</td><td>' + $(this)[0].text + '</td><td>' + $(this)[0].day_count + '</td><td>' + $(this)[0].night_count + '</td><td>' + $(this)[0].unknown_count + '</td><td>' + $(this)[0].total_count + '</td></tr>');
+                $i++;
+              }
+            });
+
+            //Output exteriors
+            $(scene_headings).each(function(){
+              if($(this)[0].text.indexOf('EXT.') < 0 && $(this)[0].text.indexOf('INT.') < 0) {
+                $('#locations table').append('<tr class="unknown"><td>' + $i + '</td><td>' + $(this)[0].text + '</td><td>' + $(this)[0].day_count + '</td><td>' + $(this)[0].night_count + '</td><td>' + $(this)[0].unknown_count + '</td><td>' + $(this)[0].total_count + '</td></tr>');
+                $i++;
+              }
+            });
+
+            /*End statistics section */          
+          });
         });
       });    
     }   
